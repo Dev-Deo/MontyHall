c6e2d9485695d922f074c3ae9cd62099299a71ca
Isuru De Silva <bhashitha88@gmail.com>
2023-08-13 09:25:27 +0530

    Migration added

---
+++ Infrastructure/Migrations/20230813035451_AddEntityRelationship.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddEntityRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GameResults_GameSetupId",
+                table: "GameResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameResults_GameSetupId",
+                table: "GameResults",
+                column: "GameSetupId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GameResults_GameSetupId",
+                table: "GameResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameResults_GameSetupId",
+                table: "GameResults",
+                column: "GameSetupId");
+        }
+    }
+}
